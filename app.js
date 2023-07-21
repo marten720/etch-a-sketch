f99ba737b3657675425f7d390a459c1b897c13df
marten720 <137708101+marten720@users.noreply.github.com>
2023-07-21 12:10:08 +0300

    Mostly edited app.js. Modified the text box that's used for user input for new grid size. Hovering now changes color of grid containers. Need to work on border disappearing when hovering over grid items and creating new grid because the size gets messed up. Also, a reset grid function was added that resets the grid every time the reset button is clicked.

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 const container = document.getElementById("container"); // accessing container
+const newSize = document.getElementById("quantity"); // new size of grid
+const btn = document.getElementsByClassName("reset"); // might not work because reset is a class not id!!!!!!!
 
-
+// creating new grid
 function createGrid(num) {
     container.style.setProperty('--grid-rows', num);
     container.style.setProperty('--grid-cols', num);
-    for (c = 0; c < (num * num); c++) {
+    for (c = 0; c < num ** 2; c++) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "grid-item";
     };
-  };
+
+    hoverColor();
+};
+
+// changes color when hovering over grid item
+function hoverColor() {
+    let items = document.querySelectorAll(".grid-item");
+    items.forEach(item => {
+        item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "black";
+
+        // changing opacity by 10%
+        if(parseFloat(item.style.opacity)) {
+            item.style.opacity = parseFloat(item.style.opacity) + 0.1;
+            } else {
+            item.style.opacity = 0.1;
+            }
+        });
+    });
+}
   
+  // initializing default grid
   createGrid(16, 16);
+
+function newGrid() {
+    if (newSize.value < 1 || newSize.value > 100)
+        newSize.value("Incorrect value!");
+    else {
+        eraseGrid();
+        createGrid(newSize.value);
+    }
+}
+
+function eraseGrid() {
+    let items = document.querySelectorAll(".grid-item");
+    items.forEach(item => {
+        item.style.backgroundColor = "white";
+    });
+}
+
+btn[0].addEventListener("click", newGrid);
